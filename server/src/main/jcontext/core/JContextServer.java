e96327a891f5c311085f08be7312dbde347933eb
vigenere <vigenere36@gmail.com>
2017-02-05 00:17:28 -0800

    initial commit: set up gradle

---
+++ server/src/main/jcontext/core/JContextServer.java
@@ -0,0 +1,12 @@
+package jcontext.core;
+
+public class JContextServer {
+    public boolean test() {
+        System.out.println("Hello world test!");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
